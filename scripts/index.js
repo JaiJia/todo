886367daaa8c7913afd3f7875a3801d102f05169
Jiawei Bao <baojiawei1995@gmail.com>
2017-11-10 16:09:58 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,7 @@ function init(spaceName) {
     renderClassify(spaceName);
     addClass($(".classify-project")[0], "project-selected");
     addClass($(".classify-task")[0], "pro-selected");
+    renderCount();
     renderTask();
     if ($(".task-item")[0]) {
         addClass($(".task-item")[0], "task-selected");
@@ -20,8 +21,6 @@ function init(spaceName) {
     renderDetail();
     // 删除分类
     $.delegate($(".classify-projects")[0], "img", "click", deleteCategory);
-
-
 }
 
 // 读取深层数据
@@ -96,6 +95,7 @@ function editTask(e) {
         $(".item-date")[1].value = $(".item-date")[0].innerText;
         $(".detail-value")[0].value = $(".detail-content")[0].innerText;
     }
+
     toggleDetail();
 }
 
@@ -137,14 +137,15 @@ function saveTask() {
     parData.value.splice(inx, 1, resData);
     localDB.update("todo", parData.name, parData);
 
-    renderTask(resData);
+    renderTask();
+    renderCount();
 
     addClass(toArray($(".task-item")).find((item) => {
         return item.innerText === taskDetail.title;
     }), "task-selected");
+
     renderDetail();
     toggleDetail();
-
 }
 
 // 渲染分类列表
@@ -182,7 +183,7 @@ function renderTask() {
     var totalData = readData(currentName);
     var resData = totalData.resData;
 
-    var taskList = resData.value;
+    var taskList = !!resData ? resData.value : [];
     var str = "";
     var dateArr = [];
     var childEle;
@@ -195,7 +196,7 @@ function renderTask() {
             str += '<li>';
             str += '<div class="task-date">' + taskList[i].date + '</div>';
             str += '<ul class="task-items">';
-            str += '<li class="task-item">' + taskList[i].title + '</li>';
+            str += '<li class="task-item item-ever">' + taskList[i].title + '</li>';
             str += '</ul>';
             str += '</li>';
             $(".task-dates")[0].innerHTML += str;
@@ -207,14 +208,22 @@ function renderTask() {
         }
     }
     // 给"已完成"添加Class
-    var doneEle = resData.value.filter((item) => {
+    var doneEle = taskList.filter((item) => {
         return item.status === "done";
     });
     for (let i = 0; i < doneEle.length; i++) {
+        removeClass(toArray($(".task-item")).find((item) => {
+            return item.innerText === doneEle[i].title;
+        }), "item-ever");
         addClass(toArray($(".task-item")).find((item) => {
             return item.innerText === doneEle[i].title;
         }), "item-done");
     }
+
+    if ($(".task-item")[0]) {
+        addClass($(".task-item")[0], "task-selected");
+    }
+    renderDetail();
 }
 
 // 渲染详情域
@@ -225,19 +234,48 @@ function renderDetail() {
     var totalData = readData(currentName);
     var resData = totalData.resData;
     var parData = totalData.parData;
-    var detailObj = resData.value.find((item) => {
-        return item.title === $(".task-selected")[0].innerText;
-    });
+    var detailObj;
 
+    if (!!resData) {
+        detailObj = resData.value.find((item) => {
+            return item.title === $(".task-selected")[0].innerText;
+        });
+    }
     if (detailObj) {
         $(".task-name")[0].innerText = detailObj.title;
         $(".item-date")[0].innerText = detailObj.date;
         $(".detail-content")[0].innerText = detailObj.particulars;
+    } else {
+        $(".task-name")[0].innerText = "";
+        $(".item-date")[0].innerText = "";
+        $(".detail-content")[0].innerText = "";
     }
+
     $(".task-name")[1].value = "";
     $(".item-date")[1].value = "";
     $(".detail-value")[0].value = "";
+}
 
+function renderCount() {
+    var numThis;
+    var allSum = 0;
+    toArray($(".classify-project")).forEach((item) => {
+        var countSum = 0;
+        toArray(item.nextSibling.children).forEach((cur) => {
+            numThis = toArray(readData(cur.firstChild.innerText.split(" ")[0]).resData.value)
+                .filter((cur) => {
+                    return cur.status === "ever";
+                }).length;
+            cur.firstChild.children[0].innerText = numThis;
+            countSum += numThis;
+        });
+        item.children[0].innerText = countSum;
+        numThis = 0;
+    });
+    toArray($(".classify-project")).forEach((item) => {
+        allSum += +item.children[0].innerText;
+    });
+    $(".classify-sum")[0].children[0].innerText = allSum;
 }
 
 // 点击切换样式
@@ -270,7 +308,10 @@ $.click($(".new-class")[0], function() {
 
 
 // 选择分类
-$.delegate($(".classify-list")[0], "div", "click", tranClass);
+$.delegate($(".classify-list")[0], "div", "click", function(e) {
+    tranClass(e);
+    renderTask();
+});
 
 // 新增任务
 $.click($(".new-task")[0], editTask);
@@ -297,35 +338,67 @@ $.click($(".status-save")[0], function() {
     localDB.update("todo", parData.name, parData);
 
     renderTask();
-});
-$.click($(".status-save")[0], function() {
-
-    var currentEle = $(".pro-selected")[0];
-    var currentName = $(".pro-selected")[0].innerText.split(" ")[0];
-    var totalData = readData(currentName);
-    var resData = totalData.resData;
-    var parData = totalData.parData;
-    var inx = parData.value.findIndex((item) => {
-        return item.name === resData.name;
-    });
-
-    var isNew = resData.value.findIndex((item) => {
-        return item.title === $(".task-name")[0].innerText || $(".task-name")[1].value;
-    });
-    resData.value[isNew].status = "done";
-    parData.value.splice(inx, 1, resData);
-    localDB.update("todo", parData.name, parData);
+    renderCount();
 });
 
 // 选择任务状态
 $.delegate($(".task-ul")[0], "li", "click", function(e) {
     transferClass($(".task-ul")[0], "status-selected", e);
+    var targetEle = e.target;
+    if (hasClass(targetEle, "task-all")) {
+        toArray($(".task-item")).forEach((item) => {
+            item.style.display = "block";
+            item.parentNode.previousSibling.style.display = "block";
+        });
+    } else if (hasClass(targetEle, "task-ever")) {
+        toArray($(".task-item")).forEach((item) => {
+
+            item.style.display = "block";
+            item.parentNode.previousSibling.style.display = "block";
+            toArray($(".item-done")).forEach((cur) => {
+                cur.style.display = "none";
+                var ind = toArray(cur.parentNode.children).findIndex((cur) => {
+                    return cur.style.display === "block";
+                });
+                if (ind === -1) {
+                    cur.parentNode.previousSibling.style.display = "none";
+                }
+            });
+        });
+    } else if (hasClass(targetEle, "task-done")) {
+        toArray($(".task-item")).forEach((item) => {
+
+            item.style.display = "block";
+            item.parentNode.previousSibling.style.display = "block";
+            toArray($(".item-ever")).forEach((cur) => {
+                cur.style.display = "none";
+                var ind = toArray(cur.parentNode.children).findIndex((cur) => {
+                    return cur.style.display === "block";
+                });
+                if (ind === -1) {
+                    cur.parentNode.previousSibling.style.display = "none";
+                }
+            });
+        });
+    }
 });
 
 // 选择任务日期
-if (!!$(".task-dates")[0].childNodes[0]) {
+if (!!$(".task-dates")[0].children[0]) {
     $.delegate($(".task-dates")[0], "li", "click", function(e) {
         transferClass($(".task-dates")[0], "task-selected", e);
         renderDetail();
     });
 }
+
+// 编辑状态点击面板自动退出
+$.click(document.body, function(e) {
+    if (!hasClass(e.target, "new-task") && $(".detail-edit")[0].style.display === "block") {
+        var ind = toArray(e.path).findIndex((item) => {
+            return item.className && hasClass(item, "detail");
+        });
+        if (ind === -1) {
+            toggleDetail();
+        }
+    }
+});
